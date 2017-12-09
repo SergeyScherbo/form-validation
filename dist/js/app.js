3494776f4bade75d45ead75e6b87895a7af4ed72
Sergey Scherbo <sergey31scherbo@gmail.com>
2017-12-09 23:16:14 +0400

    regex test for email field

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,12 +3,17 @@
 var fields = document.querySelectorAll('.field input');
 
 var validate = function validate(e) {
+  var emailReg = /[@+.]/;
   if (this.value.length < 1) {
     this.parentNode.classList.remove('field_valid');
     this.parentNode.classList.add('field_notvalid');
   } else {
     this.parentNode.classList.remove('field_notvalid');
     this.parentNode.classList.add('field_valid');
+  }
+
+  if (this.type === "email" && emailReg.test(this.value)) {
+    console.log("Your email is correct!");
   }
 };
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 const fields = document.querySelectorAll('.field input');
 
 const validate = function(e) {
+  const emailReg = /[@+.]/;
   if (this.value.length < 1) {
     this.parentNode.classList.remove('field_valid');
     this.parentNode.classList.add('field_notvalid');
@@ -8,6 +9,10 @@ const validate = function(e) {
     this.parentNode.classList.remove('field_notvalid');
     this.parentNode.classList.add('field_valid');
   }
-}
+
+  if (this.type === "email" && emailReg.test(this.value)) {
+    console.log("Your email is correct!");
+  }
+};
 
 fields.forEach(field => field.addEventListener('blur', validate));
